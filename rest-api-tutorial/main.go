cc3cd7aa5aafa0f4fd048e3b4c8412af3b1f895e
seiya_tagami <bepozurapu274@gmail.com>
2023-05-27 10:47:27 +0900

    first commit

---
+++ rest-api-tutorial/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{
+			Title: "Test Title", 
+			Desc: "Test Description", 
+			Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hi: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+type Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
