952d53a1782946800fe22a52b4a5b997e38b0d28
seiya_tagami <bepozurapu274@gmail.com>
2023-05-27 11:09:32 +0900

    done

---
+++ rest-api-tutorial/ch02/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{
+			Title: "Test Title", 
+			Desc: "Test Description", 
+			Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hi: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+type Articles []Article
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func main() {
+	handleRequests()
+}
